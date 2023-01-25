ff54d02657fe8ea777a6851f969ae13ad58c8233
loudagnese <louis.dagnese@gmail.com>
2023-01-25 05:33:10 -0800

    added handlebars

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const routes = require("./controllers/");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
 app.listen(3001, () => {
-    console.log(`API server now listening on port 3001`);
+  console.log(`API server now listening on port 3001`);
 });
